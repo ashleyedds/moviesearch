718dff165017fdf111e76c4d39ac81d63b0db8ba
Ashley Edds <ashley@edds.net>
2018-08-12 13:10:19 -0400

    styling started, no results working

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,12 +6,20 @@ $(document).ready(() => {
 
         const queryURL = `https://api.themoviedb.org/3/search/movie?query=${movieSearch}&api_key=0a9190c3d37a866d316a11a18a3eef87`;
 
+        $(".movie-display").empty();
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).done((response) => {
-            console.log(response.results)
+            if (response.results.length === 0) {
+                
+                const noResults = $("<h5>Sorry, we can't seem to find a movie that matches that search</h5>")
+
+                $(".movie-display").append(noResults)
+
+            }
+            else {
             for (let i = 0; i < response.results.length; i++) {
                 $(".plotBtn").on("click", function () {
                     $("#plotModal").modal("show")
@@ -32,9 +40,12 @@ $(document).ready(() => {
                 <button type='button' class='btn btn-primary plotBtn' data-toggle='modal' data-target='#plotModal' data-title='${movieTitle}' data-plot='${movieOverview}'>Overview</button>`);
 
                 movieCard.append(cardImg, cardBody);
+                
                 $(".movie-display").append(movieCard);
+                
 
             }
+        }
         });
     });
 
