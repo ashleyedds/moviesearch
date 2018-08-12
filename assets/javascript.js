aacc2440fe3ad6bd431f9d858b57a44263410b15
Ashley Edds <ashley@edds.net>
2018-08-12 11:54:01 -0400

    initial commit, basic functionality working

---
+++ assets/javascript.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+
+    $("#search-button").on("click", (event) => {
+        event.preventDefault();
+        const movieSearch = $("#movie-input").val().trim();
+
+        const queryURL = `https://api.themoviedb.org/3/search/movie?query=${movieSearch}&api_key=0a9190c3d37a866d316a11a18a3eef87`;
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done((response) => {
+            console.log(response.results)
+            for (let i = 0; i < response.results.length; i++) {
+                $(".plotBtn").on("click", function () {
+                    $("#plotModal").modal("show")
+                    const title = $(this).attr("data-title");
+                    const plot = $(this).attr("data-plot");
+                    $("#modalLabel").text(title)
+                    $("#modalBody").text(plot);
+                });
+
+                const movieTitle = response.results[i].title;
+                const movieOverview = response.results[i].overview;
+                const moviePoster = `http://image.tmdb.org/t/p/w342//${response.results[i].poster_path}`
+
+                const movieCard = $("<div class='card'>");
+                const cardImg = $(`<img class='card-img-top' src=${moviePoster}>`);
+                const cardBody = $(`<div class='card-body'>
+                <h5 class='card-title'>${movieTitle}</h5>
+                <button type='button' class='btn btn-primary plotBtn' data-toggle='modal' data-target='#plotModal' data-title='${movieTitle}' data-plot='${movieOverview}'>Overview</button>`);
+
+                movieCard.append(cardImg, cardBody);
+                $(".movie-display").append(movieCard);
+
+            }
+        });
+    });
+
+
+})
